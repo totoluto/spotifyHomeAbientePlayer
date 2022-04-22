937b39bb17d15af9576a607b2f65be167e424caa
totoluto <94611749+totoluto@users.noreply.github.com>
2022-04-22 15:42:17 +0200

    initial commit

---
+++ credetials.py
@@ -0,0 +1,5 @@
+SPOTIPY_CLIENT_ID = 'your-client-id'
+SPOTIPY_CLIENT_SECRET = 'your-client-secret'
+SPOTIPY_REDIRECT_URI = 'your-redirect-uri'
+SCOPE = "user-read-currently-playing user-modify-playback-state user-read-playback-state streaming"
+DEVICE_ID = 'your-device-id'
