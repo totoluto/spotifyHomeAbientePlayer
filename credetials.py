ccca165fda4c7f8dbdab4f4b76259a7d728a4fe9
totoluto <94611749+totoluto@users.noreply.github.com>
2022-12-14 11:46:47 +0100

    added missing variable

--- credetials.py
+++ credetials.py
@@ -3,3 +3,4 @@ SPOTIPY_CLIENT_SECRET = 'your-client-secret'
 SPOTIPY_REDIRECT_URI = 'your-redirect-uri'
 SCOPE = "user-read-currently-playing user-modify-playback-state user-read-playback-state streaming"
 DEVICE_ID = 'your-device-id'
+PLY_URI = 'your-playlist'
